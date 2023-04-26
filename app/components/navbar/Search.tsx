6d8c0ed04376183217209baaf5d7e89ca9003aee
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-04-26 13:03:12 +0200

    changes

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -15,7 +15,52 @@ const Search = () => {
                 cursor-pointer
             "
         >
-            
+            <div
+                className="
+                    flex
+                    flex-row
+                    items-center
+                    justify-between
+                "
+            >
+                <div
+                    className="
+                        text-sm
+                        font-semibold
+                        px-6
+                    "
+                >
+                    anywhere
+                </div>
+                <div
+                    className="
+                        hidden
+                        sm:block
+                        text-sm
+                        font-semibold
+                        px-6
+                        border-x-[1px]
+                        flex-1
+                        text-center
+                    "
+                >
+                    Any week
+                </div>
+                <div
+                    className="
+                        text-sm
+                        pl-6
+                        pr-2
+                        text-gray-600
+                        flex
+                        flex-row
+                        items-center
+                        gap-3
+                    "
+                >
+                    <div className="hidden sm:block">Add Guests</div>
+                </div>
+            </div>
         </div>
      );
 }
