1371e52c3bcf8f4678aac10d120ef6639d997015
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-04-26 12:57:34 +0200

    changes

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,6 +1,20 @@
+'use client';
+
 const Search = () => {
     return ( 
-        <div>
+        <div
+            className="
+                border-[1px]
+                w-full
+                md: w-auto
+                py-2
+                rounded-full
+                shadow-sm
+                hover:shadow-md
+                transition
+                cursor-pointer
+            "
+        >
             
         </div>
      );
