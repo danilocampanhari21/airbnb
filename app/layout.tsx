6e0d5a639f2c6b4d41833d383f9b04902ef831d8
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-04-27 20:48:09 +0200

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <Modal />
           <Navbar />
         </ClientOnly>
         {children}
