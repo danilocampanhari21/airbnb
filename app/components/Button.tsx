44895036501eee500abd5cb56464cf01fc11b142
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-04-29 18:32:15 +0200

    changes

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,3 +1,14 @@
+import { IconType } from "react-icons";
+
+interface ButtonProps {
+    label: string;
+    onClick: (e: React.MouseEvent<HTMLButtonElement>) => void:
+    disable?: boolean;
+    outline?: boolean;
+    small?: boolean;
+    icon?: IconType;
+}
+
 const Button = () => {
     return (
         <button></button>
