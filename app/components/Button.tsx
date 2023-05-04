913463c3031924efb47c8cde6daf9a004f5f4d49
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-05-04 18:36:23 +0200

    changes

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,3 +1,5 @@
+'use-client';
+
 import { IconType } from "react-icons";
 
 interface ButtonProps {
@@ -15,10 +17,12 @@ const Button: React.FC<ButtonProps> = ({
     disable,
     outline,
     small,
-    icon
+    icon: Icon
 }) => {
     return (
         <button
+            onClick={onClick}
+            disabled={disable}
             className={`
                 relative
                 disabled:cursor-not-allowed
@@ -27,10 +31,24 @@ const Button: React.FC<ButtonProps> = ({
                 transition
                 w-full
                 ${outline? 'bg-white' : 'bg-rose-500'}
-                ${outline? 'border-white' : 'border-rose-500'}
+                ${outline? 'border-black' : 'border-rose-500'}
                 ${outline? 'text-black' : 'text-white'}
+                ${small ? 'py-1' : 'py-3'}
+                ${small ? 'text-sm' : 'text-md'}
+                ${small ? 'font-light' : 'font-semibold'}
+                ${small ? 'border-[1px]' : 'border-2'}
             `}
         >
+            {Icon && (
+                <Icon 
+                    size={24}
+                    className="
+                        absolute
+                        left-4
+                        top-3
+                    "
+                />
+            )}
             {label}
         </button>
     );
